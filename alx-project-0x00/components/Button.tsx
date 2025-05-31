35686b25fd4ace91c27e0e6754bab0aa87691d4b
Ahmed Mowafy <ahmedmowafy74@GMAIL.COM>
2025-05-31 12:15:58 +0300

    fix 6

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,6 +1,6 @@
 import { ButtonProps } from "@/interfaces";
 
-const Button: React.FC<ButtonProps> = ({ title, styles = "" }) => {
+const Button: React.FC<ButtonProps> = ({ title, styles }) => {
   return (
     <button className={`bg-blue-500 text-white font-medium hover:bg-blue-600 transition-colors ${styles}`}>
       {title}
